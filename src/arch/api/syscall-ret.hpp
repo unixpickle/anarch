b0d2a97e8e80806327a28ebc01cd9ca12f62d5f5
Alex Nichol <unixpickle@gmail.com>
2014-08-25 12:00:24 -0400

    SyscallRet helpers

--- src/arch/api/syscall-ret.hpp
+++ src/arch/api/syscall-ret.hpp
@@ -18,6 +18,48 @@ union SyscallRet {
   VirtAddr virt;
   PhysSize pSize;
   size_t vSize;
+  
+  static inline SyscallRet Boolean(bool b) {
+    SyscallRet r;
+    r.boolean = b;
+    return r;
+  }
+  
+  static inline SyscallRet Integer(int i) {
+    SyscallRet r;
+    r.integer = i;
+    return r;
+  }
+  
+  static inline SyscallRet Integer32(uint32_t i) {
+    SyscallRet r;
+    r.integer32 = i;
+    return r;
+  }
+  
+  static inline SyscallRet Phys(PhysAddr p) {
+    SyscallRet r;
+    r.phys = p;
+    return r;
+  }
+  
+  static inline SyscallRet Virt(VirtAddr v) {
+    SyscallRet r;
+    r.virt = v;
+    return r;
+  }
+  
+  static inline SyscallRet PhysSize(PhysSize p) {
+    SyscallRet r;
+    r.pSize = p;
+    return r;
+  }
+  
+  static inline SyscallRet VirtSize(size_t s) {
+    SyscallRet r;
+    r.vSize = s;
+    return r;
+  }
 };
 
 }
